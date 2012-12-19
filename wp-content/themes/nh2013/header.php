6302cdf1db944ef51ca513148f68993be1aa7a12
David Iffland <david@heavyco.de>
2012-12-19 12:48:25 -0600

    Added wordpress importer

---
+++ wp-content/themes/nh2013/header.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+
+    <!-- Use the .htaccess and remove these lines to avoid edge case issues.
+       More info: h5bp.com/i/378 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title>Notebook Heavy</title>
+    <meta name="description" content="">
+
+    <link rel="author" href="http://plus.google.com/117442799415877949162" />
+    <!-- Mobile viewport optimized: h5bp.com/viewport -->
+    <meta name="viewport" content="width=device-width">
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+
+    <link rel="stylesheet" href="css/nhstyle.css">
+
+    <link rel="alternate" type="application/rss+xml" title="Notebook Heavy &raquo; Feed" href="http://notebookheavy.com/feed/" />
+    <link rel="alternate" type="application/rss+xml" title="Notebook Heavy &raquo; Comments Feed" href="http://notebookheavy.com/comments/feed/" />
+
+    <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
+    <script type="text/javascript" src="//use.typekit.net/iis6ajs.js"></script>
+    <script type="text/javascript">try { Typekit.load(); } catch (e) { }</script>
+    <script src="js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+    <header>
+        <a href="index.html" title="Notebook Heavy">NOTEBOOK HEAVY</a>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/category/words" title="words">Words</a>
+                </li>
+                <li><a href="/category/code" title="code">Code</a></li>
+                <li><a href="/about" title="about">About</a></li>
+            </ul>
+        </nav>
+
+
+    </header>
