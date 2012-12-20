18c2b40b4f9f9821df1e80ae9032580bc1c5cd56
David Iffland <david@heavyco.de>
2012-12-20 11:32:26 -0600

    update to thumbnail URL retrieve

--- wp-content/themes/nh2013/functions.php
+++ wp-content/themes/nh2013/functions.php
@@ -70,7 +70,7 @@ if ( ! function_exists( 'nhthirteen_get_sliver_source' ) ) :
 function nhthirteen_get_sliver_source( ) {
 
 	$image_id = get_post_thumbnail_id();
-	$image_url = wp_get_attachment_image_src($image_id);
+	$image_url = wp_get_attachment_image_src($image_id, 'thumbnail');
 	$image_url = $image_url[0];
 
 	echo $image_url;
