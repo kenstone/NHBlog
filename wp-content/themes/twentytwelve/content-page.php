1992d0491ff49c4defee6da1cd4dab42b92fc8cb
David Iffland <david@heavyco.de>
2012-12-21 09:56:38 -0600

    Revert "Content page template added."
    This reverts commit 0bff679d5df324dd2a516bcddfcc94c0a0ff4fd7.

--- wp-content/themes/twentytwelve/content-page.php
+++ wp-content/themes/twentytwelve/content-page.php
@@ -8,10 +8,16 @@
  */
 ?>
 
-<header class="entry-header">
-	<h1><?php the_title(); ?></h1>
-</header>
-<section>
-	<?php the_content(); ?>
-</section>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
 
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+		<footer class="entry-meta">
+			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-meta -->
+	</article><!-- #post -->
