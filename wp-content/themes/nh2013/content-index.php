38b8a2129821e1742d6514b9d984bbeaecb20df5
David Iffland <david@heavyco.de>
2013-01-05 18:47:01 -0600

    Additional Tweaks to CSS and Main Page

--- wp-content/themes/nh2013/content-index.php
+++ wp-content/themes/nh2013/content-index.php
@@ -10,7 +10,9 @@
 
 <article id="post-<?php the_ID(); ?>">
     <div style="background-image: url(<?php echo nhthirteen_get_sliver_source() ?>)" class="sliver">
-    	<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( 'Permalink to %s', the_title_attribute( 'echo=0' )  ) ); ?>" rel="bookmark">&nbsp;</a>
+        <div class="blackOverlay">
+    	   <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( 'Permalink to %s', the_title_attribute( 'echo=0' )  ) ); ?>" rel="bookmark">&nbsp;</a>
+        </div>
     </div>
     <div class="postInfo">
         <h2><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( 'Permalink to %s', the_title_attribute( 'echo=0' )  ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
