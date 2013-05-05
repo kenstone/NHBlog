dd06fe56ef76c7bb9c899c8092ec0756637ede75
David Iffland <david@heavyco.de>
2013-05-05 10:50:07 -0500

    Removed Creative Commons and more CSS
    I removed the creative commons links and changed to all rights reserved.
    Also, there was still a CSS in there from the original syntax
    highlighter. It's gone now.

--- wp-content/themes/nh2013/footer.php
+++ wp-content/themes/nh2013/footer.php
@@ -12,8 +12,7 @@
     
 
     <footer>
-        <label>&copy; 2012, <a href="http://plus.google.com/117442799415877949162/?rel=me" title="David Iffland">David Iffland</a></label>
-        <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a>
+        <label>&copy; 2012, <a href="http://plus.google.com/117442799415877949162/?rel=me" title="David Iffland">David Iffland</a> All Rights Reserved</label>
         <label>Designed in Illinois by me</label>
     </footer>
 
@@ -36,6 +35,6 @@
       })();
 
     </script>
-        
+
 </body>
 </html>
