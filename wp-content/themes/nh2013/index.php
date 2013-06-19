9b0651675589b6028e6c95f6f26739d0a5958391
David Iffland <david@heavyco.de>
2013-06-19 08:01:04 -0500

    Removed the Footer Ad

--- wp-content/themes/nh2013/index.php
+++ wp-content/themes/nh2013/index.php
@@ -23,18 +23,4 @@ get_header(); ?>
 
 </div>
 
-<div id="leaderboard">
-	<script type="text/javascript"><!--
-	google_ad_client = "ca-pub-0038909073879850";
-	/* nh13 footer */
-	google_ad_slot = "3540156985";
-	google_ad_width = 728;
-	google_ad_height = 90;
-	//-->
-	</script>
-	<script type="text/javascript"
-	src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-	</script>
-</div>
-
 <?php get_footer(); ?>
